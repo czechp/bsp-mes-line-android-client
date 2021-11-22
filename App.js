5ce39e610e647d3b6cf502b7285e0f3a90ffa7b9
czechp <webcoderc@gmail.com>
2021-11-22 10:38:38 +0100

    Implement AppText and AppInput

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppText from "./app/components/AppText/AppText";
+import AppTextInput from "./app/components/AppTextInput/AppTextInput";
 
 export default function App() {
+  const [text, setText] = useState("123");
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppTextInput
+      title="Test input"
+      value={text}
+      onChangeText={setText}
+      maxLength={5}
+      minLength={3}
+       />
     </View>
   );
 }
@@ -14,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/configuration/styles.js
@@ -0,0 +1,6 @@
+import colors from "./colors"
+
+export const fontStyles = {
+    fontSize: 25,
+    color: colors.secondary
+}
