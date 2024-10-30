59c6bc90384cb812b42867c69b710ec195e7fc90
czechp <webcoderc@gmail.com>
2024-10-30 19:47:13 +0100

    Back to old version

--- app/constants/URL.js
+++ app/constants/URL.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://10.0.6.94:8080/api/";
+export const BACKEND_URL = "http://192.168.0.109:8080/api/";
 // export const BACKEND_URL = "http://192.168.4.140:8080/api/";
